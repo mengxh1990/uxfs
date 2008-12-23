08085ac478dfdd82aeb9e5eb11d622fa68481456
warmfirefly <warmfirefly@52f5cd88-c5c6-11dd-9d8b-4f985c3f9f46>
2008-12-23 09:10:59 +0000

    can mkdir and rmdir now

--- namei.c
+++ namei.c
@@ -252,8 +252,138 @@ end_unlink:
 	return err;
 }
 
+static int uxfs_make_empty(struct inode *inode, struct inode *dir)
+{
+	struct ux_inode_info *ux_inode = uxfs_i(inode);
+	struct buffer_head *bh;
+	struct ux_dirent *de;
+	int blk, err;
+
+	blk = uxfs_new_block(inode->i_sb, &err);
+	if (err)
+		return err;
+	bh = sb_bread(inode->i_sb, blk);
+	de = (struct ux_dirent *)bh->b_data;
+	de->d_ino = inode->i_ino;
+	strcpy(de->d_name, ".");
+	de++;
+	de->d_ino = dir->i_ino;
+	strcpy(de->d_name, "..");
+	mark_buffer_dirty(bh);
+	brelse(bh);
+
+	inode->i_blocks = 1;
+	inode->i_size = sizeof(struct ux_dirent) * 2;
+	ux_inode->i_data[0] = blk;
+	mark_inode_dirty(inode);
+	return 0;
+}
+
+static int uxfs_mkdir(struct inode * dir, struct dentry *dentry, int mode)
+{
+	struct inode * inode;
+	int err = -EMLINK;
+
+	inode_inc_link_count(dir);
+
+	inode = uxfs_new_inode(dir->i_sb, &err);
+	if (!inode)
+		goto out_dir;
+
+	inode->i_mode = S_IFDIR | mode;
+	if (dir->i_mode & S_ISGID)
+		inode->i_mode |= S_ISGID;
+	uxfs_set_inode(inode);
+
+	inode_inc_link_count(inode);
+
+	err = uxfs_make_empty(inode, dir);
+	if (err)
+		goto out_fail;
+
+	err = uxfs_add_link(dentry, inode);
+	if (err)
+		goto out_fail;
+
+	d_instantiate(dentry, inode);
+out:
+	return err;
+
+out_fail:
+	inode_dec_link_count(inode);
+	inode_dec_link_count(inode);
+	iput(inode);
+out_dir:
+	inode_dec_link_count(dir);
+	goto out;
+
+	return 0;
+}
+
+/*
+ * routine to check that the specified directory is empty (for rmdir)
+ */
+int uxfs_empty_dir(struct inode * inode)
+{
+	struct ux_inode_info *ux_inode = uxfs_i(inode);
+	struct super_block *sb = inode->i_sb;
+	struct buffer_head *bh;
+	struct ux_dirent *de;
+	off_t offset = 0;
+	int i, j;
+
+	for (i = 0; i < inode->i_blocks; i++) {
+		bh = sb_bread(sb, ux_inode->i_data[i]);
+		if (!bh) {
+			printk("uxfs: unable to read dir block\n");
+			return 0;
+		}
+
+		de = (struct ux_dirent *)bh->b_data;
+		for (j = 0; j < UX_DIR_PER_BLK && offset < inode->i_size; j++, de++) {
+			if (de->d_ino) {
+				/* check for . and .. */
+				if (de->d_name[0] != '.')
+					goto not_empty;
+				if (!de->d_name[1]) {
+					if (de->d_ino != inode->i_ino)
+						goto not_empty;
+				} else if (de->d_name[1] != '.') {
+					goto not_empty;
+				} else if (de->d_name[2]) {
+					goto not_empty;
+				}
+			}
+			offset += sizeof(struct ux_dirent);
+		}
+		brelse(bh);
+	}
+
+	return 1;
+not_empty:
+	brelse(bh);
+	return 0;
+}
+
+static int uxfs_rmdir(struct inode * dir, struct dentry *dentry)
+{
+	struct inode * inode = dentry->d_inode;
+	int err = -ENOTEMPTY;
+
+	if (uxfs_empty_dir(inode)) {
+		err = uxfs_unlink(dir, dentry);
+		if (!err) {
+			inode_dec_link_count(dir);
+			inode_dec_link_count(inode);
+		}
+	}
+	return err;
+}
+
 struct inode_operations ux_dir_inode_operations = {
 	.lookup = uxfs_lookup,
 	.create = uxfs_create,
 	.unlink	= uxfs_unlink,
+	.mkdir	= uxfs_mkdir,
+	.rmdir	= uxfs_rmdir,
 };
